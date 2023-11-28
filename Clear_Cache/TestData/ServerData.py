f767ea7f45cc90a0fc3a2f30bb01b59dd1eb208e
josejrbaldonado <josejr.baldonado@dtn.com>
2023-11-27 23:49:57 -0600

    This is commit from ClearCachePROJECT Repo

--- Clear_Cache/TestData/ServerData.py
+++ Clear_Cache/TestData/ServerData.py
@@ -1,11 +1,13 @@
-import openpyxl
+# import openpyxl
+#
+# book = openpyxl.load_workbook("C:\\Users\\JOSEJR.BALDONADO\\OneDrive - DTN, LLC\\Documents\\DTNPF_Server.xlsx")
+# sheet = book.active
+# # reading from excel
+# cell = sheet.cell(row=1, column=2)
+# print(cell.value)
+# # Writing to excel
+# sheet.cell(row=2, column=2).value = "Rahul"
+# # or
+# print(sheet['A5'].value)
 
-book = openpyxl.load_workbook("C:\\Users\\JOSEJR.BALDONADO\\OneDrive - DTN, LLC\\Documents\\DTNPF_Server.xlsx")
-sheet = book.active
-# reading from excel
-cell = sheet.cell(row=1, column=2)
-print(cell.value)
-# Writing to excel
-sheet.cell(row=2, column=2).value = "Rahul"
-# or
-print(sheet['A5'].value)
+# This is Github Simulation of PULL.
